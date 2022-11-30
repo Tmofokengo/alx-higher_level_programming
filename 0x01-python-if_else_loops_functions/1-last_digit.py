e06fb4631f34e4ccb8014df187776fe6c9c28ca2
Teboho <teboho.mofokeng@hotmail.com>
2022-11-30 11:29:07 -0800

    altered backlash for line break

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if x > 5:
 elif x == 0:
     print("Last digit of {:d} is {:d} and is equal to 0".format(number, x))
 elif x < 6 and x != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(num\
-ber, x))
+    print("Last digit of {:d} is {:d} and is less than 6 and \
+not 0".format(number, x))
