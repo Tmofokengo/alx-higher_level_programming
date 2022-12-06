b4bd0425be9f0c1f4e4c0eb8823d02aa8ebb3b59
Teboho <teboho.mofokeng@hotmail.com>
2022-12-06 01:37:53 -0800

    last tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,78 +1,72 @@
 #include "lists.h"
+
 /**
- * is_palindrome - Determines if a linked list is a palindrome
- * @head: Pointer to the entire linked list
- * Return: 1 if it is a palindrome, otherwise 0
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = NULL, *aux = NULL, *aux2 = NULL;
-	int size = 0, half = 0, i = 0, pal = 1;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (*head == NULL)
-		return (pal);
-	size = size_list(*head);
-	if (size == 1)
-		return (pal);
-	if (size == 2)
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
 	{
-		if ((*head)->n != (*head)->next->n)
-			return (0);
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
 		else
-			return (pal);
+			return (0);
 	}
-	half = size / 2;
-	aux = *head;
-	while (i < half)
-	{
-		i++;
-		add_node_at_head(&h, aux->n);
-		aux = aux->next;
-	}
-	if (size % 2 != 0)
-		aux = aux->next;
-	aux2 = h;
-	while (aux != NULL)
-	{
-		if (aux->n != aux2->n)
-			pal = 0;
-		aux = aux->next;
-		aux2 = aux2->next;
-	}
-	free_listint(h);
-	return (pal);
-}
-/**
- * size_list - Gets the size of a list
- * @head: pointer to the first node of the structure
- * Return: Number of nodes
- */
-int size_list(listint_t *head)
-{
-	int i = 0;
 
-	while (head != NULL)
-	{
-		i++;
-		head = head->next;
-	}
-	return (i);
-}
-/**
- * add_node_at_head - add nodes from the original list as stack
- * @head: Structure of the stack
- * @n: Number to be added
- *Return: The created node
- */
-listint_t *add_node_at_head(listint_t **head, int n)
-{
-	listint_t *newnode;
+	if (!dup)
+		return (1);
 
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-		return (NULL);
-	newnode->n = n;
-	newnode->next = *head;
-	*head = newnode;
-	return (*head);
+	return (0);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,10 +18,12 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
-int size_list(listint_t *head);
-listint_t *add_node_at_head(listint_t **head, int n);
+
 #endif /* LISTS_H */
